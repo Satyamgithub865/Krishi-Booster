20a0263d548789228a38e46e236bda47204e68ea
Satyam Kumar <103940888+Satyamgithub865@users.noreply.github.com>
2023-10-17 09:41:01 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-    const URL = 'mongodb://satyam865:krishibooster@ac-yzwd2xv-shard-00-00.tcvancz.mongodb.net:27017,ac-yzwd2xv-shard-00-01.tcvancz.mongodb.net:27017,ac-yzwd2xv-shard-00-02.tcvancz.mongodb.net:27017/?ssl=true&replicaSet=atlas-1ctpxd-shard-0&authSource=admin&retryWrites=true&w=majority'
+    const URL = ''
 
     try {
         await mongoose.connect(URL, { useNewUrlParser: true });
